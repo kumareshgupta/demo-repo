f518325a09d7ff0071ae1671dcd6e5b107e56629
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2021-04-16 16:02:44 +0530

    created another demo for github

---
+++ Day2 Assignments/src/com/assignment/one/PrepaidCard.java
@@ -0,0 +1,19 @@
+package com.assignment.one;
+
+public class PrepaidCard {
+	private int cardNo;
+	private double availableBalance;
+	public int getCardNo() {
+		return cardNo;
+	}
+	public void setCardNo(int cardNo) {
+		this.cardNo = cardNo;
+	}
+	public double getAvailableBalance() {
+		return availableBalance;
+	}
+	public void setAvailableBalance(double availableBalance) {
+		this.availableBalance = availableBalance;
+	}
+	
+}
