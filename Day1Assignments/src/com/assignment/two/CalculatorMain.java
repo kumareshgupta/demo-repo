b54e429df35b482c75cce38c61c179ea8c98dad5
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2021-04-16 15:58:31 +0530

    created demo assignment for github repository

---
+++ Day1Assignments/src/com/assignment/two/CalculatorMain.java
@@ -0,0 +1,10 @@
+package com.assignment.two;
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is calculator demo...");
+	}
+
+}
